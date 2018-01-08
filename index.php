0c87aadabc56436f8f83b4e90c5738c66e879f68
Shamim <35076893+sphp@users.noreply.github.com>
2018-01-09 00:42:59 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,37 @@
 <?php
-/* 
- * html function
- * $tag param is html tag name
- * $doc param is html or string inside tag / input Placeholder (array or single)
- * $opt param is html tag attrbibutes  (array)
+/*
+ * HTML function:
+ * 1st parameter is html tag name
+ * 2nd parameter is html tag attributes (supported type array or JSON)
+ * 3rd parameter is inner html or any content (supported type array or string)
  */
- 
-$button   = html("button","Submit");
-$inputs[] = html("input", "Username",	['type'=>'text','name'=>'username']);
-$inputs[] = html("input", "Full Name",	['type'=>'text','name'=>'fullname']);
-$inputs[] = html("input", "Email",	['type'=>'email','name'=>'email']);
-$inputs[] = html("input", "password",	['type'=>'password','name'=>'password']);
-$inputs[] = html("input", "",		['type'=>'button','name'=>'Submit','value'=>'Submit']);
-
-echo html("form", $inputs, ['mathod'=>'post','action'=>'']);
-
-function html($tag,$doc=[],$opt=[]){
-	$autocloseTags = ['img', 'br', 'hr', 'input', 'area', 'link', 'meta', 'param'];
-
-	$_html = "<".$tag;
-	if(!empty($opt))
-		foreach($opt as $key=>$val) $_html .= " $key=\"$val\"";
-
-	if(in_array($tag, $autocloseTags)){
-		if(!empty($doc)) $_html .= " placeholder=\"$doc\" />";
-		else $_html .= " />";
-	}else{
-		if(is_array($doc)) $_html .= " >" . implode(PHP_EOL, $doc);
-		else $_html .= " >".$doc."</$tag>";
+function html($tag, $_attr="", $_html=""){
+	$autocloseTags = ["img", "br", "hr", "input", "area", "link", "meta", "param"];
+	if(!is_array($_attr))
+		$_attr = json_decode($_attr,true);
+	$dom = "<".$tag;
+	if(!empty($_attr)){
+		foreach($_attr as $key=>$val)
+			$dom .= " $key=\"$val\"";
 	}
-	return $_html;
+	if(in_array($tag, $autocloseTags) ){
+		$dom .= " />";
+	}else{
+		if(is_array($_html))
+			$dom .= " >" . implode('', $_html) . "</$tag>";
+		else
+			$dom .= " >".$_html."</$tag>";
+	}
+	return $dom;
 }
+
+$button   = html("button","Submit");
+$inputs[] = html("input", '{"type":"text","name":"username"}');
+$inputs[] = html("input", '{"type":"text","name":"fullname"}');
+$inputs[] = html("input", '{"type":"email","name":"email"}');
+$inputs[] = html("input", '{"type":"password","name":"password"}');
+$inputs[] = html("input", ['type'=>'button','name'=>'Submit','value'=>'Submit']);
+
+echo html("form",['mathod'=>'post','action'=>''], $inputs);
 
 ?>
